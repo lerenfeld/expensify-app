f37d4816fc3bf981a9a690c55b3d094fd11a4ba6
Gal Lerenfeld <gal@giv-solutions.com>
2022-04-11 00:12:22 +0300

    Setup production webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,58 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
-module.exports = {
-  entry: './src/app.js',
-  output: {
-    path: path.join(__dirname, 'public'),
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [{
-            loader: 'babel-loader',
-            test: /\.(js|jsx)$/,
-            exclude: /(node_modules|bower_components)/,
-            options: { presets: ['@babel/env','@babel/preset-react'] }
-        },
-        {
-          test: /\.s?css$/ , 
-          use: [
-            'style-loader' ,
-            'css-loader' , 
-            'sass-loader'
-          ]
-        }
-    ]
-  },
-  devtool:'eval-cheap-module-source-map' , 
-  devServer: {
-    static: {
-      directory: path.join(__dirname, 'public')
+module.exports = (env, args) => {
+
+  const isProduction = args.mood === 'production'
+
+  return {
+    entry: './src/app.js',
+    output: {
+      path: path.join(__dirname, 'public'),
+      filename: 'bundle.js'
     },
-    
-    historyApiFallback : true,
-    compress: true,
-  },
-  mode: 'development'
+    module: {
+      rules: [{
+        loader: 'babel-loader',
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        options: { presets: ['@babel/env', '@babel/preset-react'] }
+      },
+      {
+        test: /\.s?css$/,
+        use: [ MiniCssExtractPlugin.loader, 
+          {
+              loader: 'css-loader',
+              options: {
+                  sourceMap: true
+              }
+          },
+          {
+              loader: 'sass-loader',
+              options: {
+                  sourceMap: true
+              }
+          }
+      ],
+      }
+      ]
+    },
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: 'styles.css'
+      })
+    ],
+    devtool: isProduction ? 'source-map' : 'inline-cheap-module-source-map',
+    devServer: {
+      static: {
+        directory: path.join(__dirname, 'public')
+      },
+
+      historyApiFallback: true,
+      compress: true,
+    },
+    mode: 'development'
+  };
 };
+
+
